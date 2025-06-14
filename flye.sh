d91df30bcd4270b7281569cff30f1b4680e992a3
Bright_Boamah <briteboafo@icloud.com>
2025-06-14 16:53:29 -0600

    Update flye.sh

--- flye.sh
+++ flye.sh
@@ -2,31 +2,24 @@
 
 set -e
 
-mkdir -p assemblies_flye_20 assemblies_flye_40 assemblies_flye_60 assemblies_flye_80 assemblies_flye_100
+# Usage: ./run_flye.sh /path/to/input_folder /path/to/output_folder
 
-DIR="/home/simon/Documents/assembly-dvel/Data_1"
+INPUT_DIR="$1"
+OUTPUT_DIR="$2"
 
-# Define the input files array
-OUTPUT1="/home/simon/Documents/assembly-dvel/Data_1/assemblies_flye_20"
-OUTPUT2="/home/simon/Documents/assembly-dvel/Data_1/assemblies_flye_40"
-OUTPUT3="/home/simon/Documents/assembly-dvel/Data_1/assemblies_flye_60"
-OUTPUT4="/home/simon/Documents/assembly-dvel/Data_1/assemblies_flye_80"
-OUTPUT5="/home/simon/Documents/assembly-dvel/Data_1/assemblies_flye_100"
+if [[ -z "$INPUT_DIR" || -z "$OUTPUT_DIR" ]]; then
+    echo "Usage: $0 <input_folder> <output_folder>"
+    exit 1
+fi
 
-threads=6
+mkdir -p "$OUTPUT_DIR"
 
-flye --nano-raw /home/simon/Documents/assembly-dvel/Data_1/depths/subsampled_20x.fastq --threads "$threads" --iterations 2 --genome-size 6073000 --out-dir $OUTPUT1
-flye --nano-raw /home/simon/Documents/assembly-dvel/Data_1/depths/subsampled_40x.fastq --threads "$threads" --iterations 2 --genome-size 6073000 --out-dir $OUTPUT2
-flye --nano-raw /home/simon/Documents/assembly-dvel/Data_1/depths/subsampled_60x.fastq --threads "$threads" --iterations 2 --genome-size 6073000  --out-dir $OUTPUT3
-flye --nano-raw /home/simon/Documents/assembly-dvel/Data_1/depths/subsampled_80x.fastq --threads "$threads" --iterations 2 --genome-size 6073000 --out-dir $OUTPUT4
-flye --nano-raw /home/simon/Documents/assembly-dvel/Data_1/depths/subsampled_100x.fastq --threads "$threads" --iterations 2 --genome-size 6073000  --out-dir $OUTPUT5
+for fq in "$INPUT_DIR"/*.fastq; do
+    sample_name=$(basename "$fq" .fastq)
+    out_sample_dir="$OUTPUT_DIR/${sample_name}_flye"
+    mkdir -p "$out_sample_dir"
+    echo "Running Flye for $fq..."
+    flye --nano-raw "$fq" --out-dir "$out_sample_dir" --threads 8
+done
 
-#"flye --nano-hq /home/simon/Documents/assembly-dvel/Data_1/subsampling_tryc/depth_30/B6_30.fastq --threads "$threads" --iterations 2 --genome-size 5200000 --out-dir $OUTPUT/out6
-#flye --nano-hq /home/simon/Documents/assembly-dvel/Data_1/subsampling_tryc/depth_30/B7_30.fastq --threads "$threads" --iterations 2 --genome-size 5200000 --out-dir $OUTPUT2/out7
-#flye --nano-hq /home/simon/Documents/assembly-dvel/Data_1/subsampling_tryc/depth_30/B8_30.fastq --threads "$threads" --iterations 2 --genome-size 5200000 --out-dir $OUTPUT2/out8
-#flye --nano-hq /home/simon/Documents/assembly-dvel/Data_1/subsampling_tryc/depth_30/B9_30.fastq --threads "$threads" --iterations 2 --genome-size 5200000 --out-dir $OUTPUT2/out9
-#flye --nano-hq /home/simon/Documents/assembly-dvel/Data_1/subsampling_tryc/depth_30/B10_30.fastq --threads "$threads" --iterations 2 --genome-size 5200000 --out-dir $OUTPUT2/out10
-#flye --nano-hq /home/simon/Documents/assembly-dvel/Data_1/subsampling_tryc/depth_30/B11_30.fastq --threads "$threads" --iterations 2 --genome-size 5200000 --out-dir $OUTPUT2/out11
-#flye --nano-hq /home/simon/Documents/assembly-dvel/Data_1/subsampling_tryc/depth_30/B12_30.fastq --threads "$threads" --iterations 2 --genome-size 5200000 --out-dir $OUTPUT2/out12"
-
-
+echo "All Flye assemblies are complete."
